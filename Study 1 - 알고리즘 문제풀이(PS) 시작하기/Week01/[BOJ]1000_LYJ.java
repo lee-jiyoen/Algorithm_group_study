8e94977b01879aa351dd575ee0ffbe2d977109f5
L-Y-Jeong <124586036+L-Y-Jeong@users.noreply.github.com>
2023-11-28 18:23:02 +0900

    Add files via upload

---
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week01/[BOJ]1000_LYJ.java
@@ -0,0 +1,22 @@
+package baek_1000;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class [BOJ]1000_LYJ {
+	public static void main(String[] args) throws IOException{
+		BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(buffer.readLine());
+		int num = Integer.parseInt(st.nextToken());
+		
+		//재귀....
+		
+		
+
+		System.out.println(n);
+		
+	}
+
+}
