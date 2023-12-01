f6e6901bee97055ef51f4dacaf8d98831eaea86d
choihywon <144045451+choihywon@users.noreply.github.com>
2023-12-01 20:25:14 +0900

    Add files via upload

---
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week02/[BOJ]2558_CHW.java
@@ -0,0 +1,18 @@
+package backjoon_2558;
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int num1 = Integer.parseInt(br.readLine());
+		int num2 = Integer.parseInt(br.readLine());
+		
+		System.out.println(num1+num2);
+		
+	}
+
+}
