d1c181dbf21f1958f002ee65d436b4a7cc131de4
Hyewon <hooho931@gmail.com>
2023-12-09 22:38:55 +0900

    Create [BOJ]10951_LHY.java

---
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week03/[BOJ]10951_LHY.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		
+		String str;
+		
+		while((str=br.readLine()) != null) {
+			st = new StringTokenizer(str, " ");
+		
+			  int A = Integer.parseInt(st.nextToken());
+		        int B = Integer.parseInt(st.nextToken());
+		         
+		       sb.append(A+B).append("\n");
+		}
+	
+       
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
