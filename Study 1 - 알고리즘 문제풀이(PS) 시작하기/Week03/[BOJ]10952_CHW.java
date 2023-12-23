0f62c5a0267895a73838bbc38df5ed89d592218a
choihywon <144045451+choihywon@users.noreply.github.com>
2023-12-23 10:47:53 +0900

    Create [BOJ]10952_CHW.java

---
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week03/[BOJ]10952_CHW.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (true) {
+            String[] input = br.readLine().split(" ");
+            int A = Integer.parseInt(input[0]);
+            int B = Integer.parseInt(input[1]);
+
+            if (A == 0 && B == 0) {
+                break;
+            }
+
+            System.out.println(A + B);
+        }
+	}
+
+}
