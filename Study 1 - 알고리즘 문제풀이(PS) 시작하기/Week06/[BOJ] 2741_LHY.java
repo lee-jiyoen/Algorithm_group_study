cb83a8afaf17a0bc5cc5d0aa4a5906eea7994be1
Hyewon <hooho931@gmail.com>
2024-01-20 18:11:31 +0900

    Create [BOJ] 2741_LHY.java

---
+++ Study 1 - 알고리즘 문제풀이(PS) 시작하기/Week06/[BOJ] 2741_LHY.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String N = br.readLine(); 
+		int count = Integer.parseInt(N);
+		
+		for(int i=1; i<=count; i++) {
+			bw.write(i+"\n");
+		}
+		
+		bw.flush();
+		bw.close();
+	}
+}
